43142e5e8da34615677f81013318109e86fd85c0
Lucas Brist <lucastbrist@gmail.com>
2025-09-11 16:22:58 -0500

    initial commit

---
+++ src/main/java/com/ltb/user_microservice/models/User.java
@@ -0,0 +1,32 @@
+package com.ltb.user_microservice.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String fullName;
+}
